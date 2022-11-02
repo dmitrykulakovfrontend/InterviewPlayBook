9c728c6289b7af69df562451105d679ca5f676a6
Dmitry Kulakov <atomeistee@gmail.com>
2022-11-02 15:53:45 +0300

    initial next.js with trpc, prisma and tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { trpc } from "utils/trpc";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <Component {...pageProps} />
+    </div>
+  );
 }
+export default trpc.withTRPC(App);
---
+++ server/routers/_app.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+import { signUpSchema } from "../../utils/validations";
+import { TRPCError } from "@trpc/server";
+import {hash} from 'bcrypt'
+
+export const appRouter = router({
+  signUp: publicProcedure
+    .input(signUpSchema)
+    .mutation(async ({ input, ctx }) => {
+      const { username, email, password } = input;
+      
+      const exists = await ctx.prisma.user.findFirst({ where: { email } });
+
+      if (exists) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "User already exists.",
+        });
+      }
+
+      const hashedPassword = await hash(password,10);
+
+      const result = await ctx.prisma.user.create({
+        data: { username, email, password: hashedPassword },
+      });
+
+      return {
+        status: 201,
+        message: "Account created successfully",
+        result: result.email,
+      };
+    }),
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
