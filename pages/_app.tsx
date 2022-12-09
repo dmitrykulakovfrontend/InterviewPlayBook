08b506c775e442ccdb154450ea08f946f829654e
Dmitry Kulakov <atomeistee@gmail.com>
2022-12-09 18:48:53 +0300

    chore: add Audiences analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
 config.autoAddCss = false;
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <ToastContainer position={"top-center"} autoClose={2000} />
         <Component {...pageProps} />
+        <Analytics />
       </QueryClientProvider>
     </SessionProvider>
   );
